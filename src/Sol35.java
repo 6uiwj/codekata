720b7b3cc88ef9d261b97f03b944f00692297be5
ming <oterges00@gmail.com>
2025-09-16 11:18:13 +0900

    250916

--- src/Sol35.java
+++ src/Sol35.java
@@ -1,2 +1,14 @@
 public class Sol35 {
+    public static void main(String[] args) {
+        long money = 20;
+        int price = 3;
+        int count = 4;
+        long answer = 0;
+        long m = money;
+        for(int i = 1; i<=count; i++) { //1234
+            m -= price*i; //20-30
+        }
+        answer = m >= 0 ? 0 : m*(-1);
+        System.out.println(answer);
+    }
 }
